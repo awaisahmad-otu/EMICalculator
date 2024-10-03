fe9db7c7615b472e41a9f85663423b728f093153
Awais <awais.ahmad@ontariotechu.net>
2024-10-02 22:36:54 -0400

    .

--- app/src/main/java/com/example/emicalculator/MainActivity.java
+++ app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.emicalculator;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
